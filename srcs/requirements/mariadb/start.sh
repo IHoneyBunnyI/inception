f81d07339e749b5c8a4076b5726c26bc1049a5d0
IHoneyBunnyI <kasim.baybikov@gmail.com>
2021-06-10 13:08:46 +0300

    testing $

--- srcs/requirements/mariadb/start.sh
+++ srcs/requirements/mariadb/start.sh
@@ -6,15 +6,15 @@ rc-service mariadb start
 
 mysql -u root --skip-password -e "CREATE DATABASE wordpress;"
 
-mysql -u root --skip-password -e "CREATE USER 'rvernon'@'%' IDENTIFIED BY 'pass';"
+mysql -u root --skip-password -e "CREATE USER $MYSQL_ROOT_USER@'%' IDENTIFIED BY $MYSQL_ROOT_PASSWORD;"
 
-mysql -u root --skip-password -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'rvernon'@'%';"
+mysql -u root --skip-password -e "GRANT ALL PRIVILEGES ON wordpress.* TO $MYSQL_ROOT_USER@'%';"
 
 mysql -u root --skip-password -e "FLUSH PRIVILEGES;"
 
 mysql -u root wordpress < wordpress.sql
 
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'pass';"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY $MYSQL_ROOT_PASSWORD;"
 
 rc-service mariadb stop
 
